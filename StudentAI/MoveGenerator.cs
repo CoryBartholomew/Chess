5ec328cb2f16dfd6db91aac092d7a262134d156a
TomAwesome <camargothomas@gmail.com>
2013-09-25 19:01:41 -0600

    added knight bishop and queens moves

--- StudentAI/MoveGenerator.cs
+++ StudentAI/MoveGenerator.cs
@@ -24,15 +24,18 @@ namespace StudentAI
                             switch (board[x,y])
                             {
                                     case ChessPiece.WhiteBishop:
+                                    BishopMoves(board,x,y,color);
                                     break;
                                     case ChessPiece.WhiteQueen:
+                                    QueenMoves(board,x,y,color);
                                     break;
                                     case ChessPiece.WhiteKing:
                                     break;
+                                    case ChessPiece.WhiteKnight:
+                                    KnightMoves(board,x,y, color);
+                                    break;
                                     case ChessPiece.WhitePawn:
                                     PawnMoves(board,x,y,color);
-                                    break;
-                                    case ChessPiece.WhiteKnight:
                                     break;
                                     case ChessPiece.WhiteRook:
                                     RookMoves(board,x,y,color);
@@ -42,7 +45,26 @@ namespace StudentAI
                     }
                     else
                     {
-                        
+                        switch (board[x, y])
+                        {
+                            case ChessPiece.BlackBishop:
+                                BishopMoves(board, x, y, color);
+                                break;
+                            case ChessPiece.BlackQueen:
+                                QueenMoves(board, x, y, color);
+                                break;
+                            case ChessPiece.BlackKing:
+                                break;
+                            case ChessPiece.BlackKnight:
+                                KnightMoves(board, x, y, color);
+                                break;
+                            case ChessPiece.BlackPawn:
+                                PawnMoves(board, x, y, color);
+                                break;
+                            case ChessPiece.BlackRook:
+                                RookMoves(board, x, y, color);
+                                break;
+                        }
                     }
                 }
             }
@@ -165,19 +187,152 @@ namespace StudentAI
 
         }
 
-        private void KnightMoves(ChessBoard board, int x, int y)
+        private void KnightMoves(ChessBoard board, int x, int y, ChessColor color)
         {
+            ChessColor killColor = ChessColor.White;
+            if (color == ChessColor.White)
+            {
+                killColor = ChessColor.Black;
+            }
+            int i = x - 2;
+            int j = y - 1;
+            //up left direction
+            if(i > 0 && j > 0 && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x,y),new ChessLocation(i,j) ));
+            }
+            // up right direction
+            j = y + 1;
+            if (i > 0 && j < ChessBoard.NumberOfColumns && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+            // right up direction
+            j = y + 2;
+            i = x - 1;
+            if (i > 0 && j < ChessBoard.NumberOfColumns && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+            // right down direction
+            i = x + 1;
+            if (i < ChessBoard.NumberOfRows && j < ChessBoard.NumberOfColumns && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+            // down left direction
+            i = x + 2;
+            j = y - 1;
+            if (i < ChessBoard.NumberOfRows && j > 0 && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+            //down right direction
+            j = y + 1;
+            if (i < ChessBoard.NumberOfRows && j < ChessBoard.NumberOfColumns && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+            // left up direction
+            i = x - 2;
+            j = y - 1;
+            if (i > 0 && j > 0 && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+            // left down direction
+            j = y + 1;
+            if (i > 0 && j < ChessBoard.NumberOfColumns && (board[x, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[x, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+            {
+                Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+            }
+
 
         }
 
-        private void BishopMoves(ChessBoard board, int x, int y)
+        private void BishopMoves(ChessBoard board, int x, int y, ChessColor color)
         {
-
+            ChessColor killColor = ChessColor.White;
+            if (color == ChessColor.White)
+            {
+                killColor = ChessColor.Black;
+            }
+            int i = x - 1;
+            int j = y - 1;
+            while(i > 0 && j > 0) // up left direction
+            {
+                if(board[i,j] == ChessPiece.Empty)
+                {
+                    Moves.Add(new ChessMove(new ChessLocation(x,y),new ChessLocation(i,j) ));
+                }
+                else
+                {
+                    if ((board[i, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[i, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+                    {
+                        Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(x, j)));
+                    }
+                    break;
+                }
+                i--;
+                j--;
+            }
+            while (i > 0 && j < ChessBoard.NumberOfColumns) // up rifht direction
+            {
+                if (board[i, j] == ChessPiece.Empty)
+                {
+                    Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+                }
+                else
+                {
+                    if ((board[i, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[i, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+                    {
+                        Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(x, j)));
+                    }
+                    break;
+                }
+                i--;
+                j++;
+            }
+            while (i < ChessBoard.NumberOfRows && j > 0) // down left direction
+            {
+                if (board[i, j] == ChessPiece.Empty)
+                {
+                    Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+                }
+                else
+                {
+                    if ((board[i, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[i, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+                    {
+                        Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(x, j)));
+                    }
+                    break;
+                }
+                i++;
+                j--;
+            }
+            while (i < ChessBoard.NumberOfRows && j < ChessBoard.NumberOfColumns) // up left direction
+            {
+                if (board[i, j] == ChessPiece.Empty)
+                {
+                    Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(i, j)));
+                }
+                else
+                {
+                    if ((board[i, j] > ChessPiece.Empty && killColor == ChessColor.White) || (board[i, j] < ChessPiece.Empty && killColor == ChessColor.Black))
+                    {
+                        Moves.Add(new ChessMove(new ChessLocation(x, y), new ChessLocation(x, j)));
+                    }
+                    break;
+                }
+                i++;
+                j++;
+            }
         }
 
-        private void QueenMoves(ChessBoard board, int x, int y)
+        private void QueenMoves(ChessBoard board, int x, int y, ChessColor color)
         {
-
+            RookMoves(board,x,y,color);
+            BishopMoves(board,x,y, color);
         }
 
         private void KingMoves(ChessBoard board, int x, int y)
